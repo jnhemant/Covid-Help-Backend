4638a6a9ccdd5bbbd2d7a54df9dbe02f0c9d7fb4
Hemant Jain <jnhemant319@gmail.com>
2021-06-06 22:50:12 +0530

    Removed commented lines

--- src/main/java/com/covidHelp/demo/CovidHelpApplication.java
+++ src/main/java/com/covidHelp/demo/CovidHelpApplication.java
@@ -3,18 +3,12 @@ package com.covidHelp.demo;
 import com.covidHelp.demo.repository.UserRepository;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @Import({ElasticSearchConfig.class})
-// @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})     
 @EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class CovidHelpApplication {
 
